31cbc0051d77ee00adca064341ee756de80b99b8
Victor Massao <victor.massao@cerc.inf.br>
2020-03-27 17:09:57 -0300

    Adding lombok

--- src/main/java/com/bandtec/gespospring/model/Employee.java
+++ src/main/java/com/bandtec/gespospring/model/Employee.java
@@ -1,80 +1,27 @@
 package com.bandtec.gespospring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Employee extends AbstractModel{
 
     private String cpf;
-
     private String name;
-
     private String password;
-
     private String email;
-
     private Double hourValue;
 
     @Temporal(TemporalType.DATE)
     private Date dtBirth;
 
-    public Employee() {}
-
-    public Employee(String cpf, String name, String password, String email, Double hourValue, Date dtBirth) {
-        this.cpf = cpf;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.hourValue = hourValue;
-        this.dtBirth = dtBirth;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getHourValue() {
-        return hourValue;
-    }
-
-    public void setHourValue(Double hourValue) {
-        this.hourValue = hourValue;
-    }
-
-    public Date getDtBirth() {
-        return dtBirth;
-    }
-
-    public void setDtBirth(Date dtBirth) {
-        this.dtBirth = dtBirth;
-    }
 }
--- src/main/java/com/bandtec/gespospring/model/Tasks.java
+++ src/main/java/com/bandtec/gespospring/model/Tasks.java
@@ -1,50 +1,23 @@
 package com.bandtec.gespospring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Tasks extends AbstractModel {
 
     private String title;
-
     private String description;
-
     private Integer deadline;
-
     private Double percentProject;
 
-    public Tasks() {}
-
-    public Tasks(String title, String description, Integer deadline, Double percentProject) {
-        this.title = title;
-        this.description = description;
-        this.deadline = deadline;
-        this.percentProject = percentProject;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public Integer getDeadline() {
-        return deadline;
-    }
-    public void setDeadline(Integer deadline) {
-        this.deadline = deadline;
-    }
-    public Double getPercentProject() {
-        return percentProject;
-    }
-    public void setPercentProject(Double percentProject) {
-        this.percentProject = percentProject;
-    }
 }
