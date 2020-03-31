b26690194ef75550ce7b55f83388fce5eb370766
Victor Massao <victor.massao@cerc.inf.br>
2020-03-30 21:21:38 -0300

    Adding Office and permission entities

---
+++ src/main/java/com/bandtec/gespospring/model/Permission.java
@@ -0,0 +1,19 @@
+package com.bandtec.gespospring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Permission extends AbstractModel {
+
+    private String dsPermission;
+
+}
