525f073d7cee5e79f6ee300dfc84c493a824e727
Victor Massao <victor.massao@cerc.inf.br>
2020-03-30 21:25:40 -0300

    Creating update, delete and find

--- src/main/java/com/bandtec/gespospring/controller/CategoryController.java
+++ src/main/java/com/bandtec/gespospring/controller/CategoryController.java
@@ -3,10 +3,9 @@ package com.bandtec.gespospring.controller;
 import com.bandtec.gespospring.model.Category;
 import com.bandtec.gespospring.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -20,5 +19,38 @@ public class CategoryController {
             @RequestBody Category category
     ){
         return categoryRepository.save(category);
+    }
+
+    @GetMapping("/search-all")
+    public List<Category> searchAll(){
+        return  categoryRepository.findAll();
+    }
+
+    @GetMapping("/search/{id}")
+    public Category searchById(
+            @RequestParam(name = "id") Integer id
+    ){
+        return  categoryRepository.findById(id).get();
+    }
+
+    @PutMapping("/update/{id}")
+    public Category updateCategory(
+            @RequestBody Category category,
+            @PathVariable Integer id
+    ) {
+        return categoryRepository.findById(id).map(cat -> {
+            cat.setDsCategory(category.getDsCategory());
+            return categoryRepository.save(cat);
+        }).orElseGet(() -> {
+            category.setId(id);
+            return categoryRepository.save(category);
+        });
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(
+            @RequestParam(name = "id") Integer id
+    ){;
+        categoryRepository.deleteById(id);
     }
 }
