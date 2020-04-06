5bedcb901128ae36bb1b0b9497debea9499de9d1
Victor Massao <victor.massao@cerc.inf.br>
2020-04-06 17:17:54 -0300

    Simple login

--- src/main/java/com/bandtec/gespospring/repository/EmployeeRepository.java
+++ src/main/java/com/bandtec/gespospring/repository/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.bandtec.gespospring.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-//    Employee findByPasswordAndCpf(String password, String cpf);
+    Employee findByPasswordAndCpf(String password, String cpf);
 //
 //    Employee findByPassword(String password);
 
--- src/main/java/com/bandtec/gespospring/repository/OfficeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bandtec.gespospring.repository;
-
-import com.bandtec.gespospring.model.Office;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfficeRepository extends JpaRepository<Office, Integer> {
-}
