549ea12e07f6c49c15ad9c52c28e1c94cdfbbd51
Victor Massao <victor.massao@cerc.inf.br>
2020-05-04 19:26:56 -0300

    Adding CrossOrigin with parameters allowCredentials and origins

--- src/main/java/com/bandtec/gespospring/controller/WorkScheduleController.java
+++ src/main/java/com/bandtec/gespospring/controller/WorkScheduleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/work-schedules")
 public class WorkScheduleController {
