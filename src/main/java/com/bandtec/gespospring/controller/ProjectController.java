69b1e137ac6705722f9dcc48592b846ea2c58726
Victor Massao <victor.massao@cerc.inf.br>
2020-05-04 19:26:56 -0300

    Adding CrossOrigin with parameters allowCredentials and origins

--- src/main/java/com/bandtec/gespospring/controller/ProjectController.java
+++ src/main/java/com/bandtec/gespospring/controller/ProjectController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
 
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
