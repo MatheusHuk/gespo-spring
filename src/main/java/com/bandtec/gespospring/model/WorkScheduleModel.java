02657db181502e27307600d60624923d7b287c37
Victor Massao <victor.massao@cerc.inf.br>
2020-05-05 16:56:45 -0300

    Adding project and employee in work-schedule by employeeId endpoint

--- src/main/java/com/bandtec/gespospring/model/WorkScheduleModel.java
+++ src/main/java/com/bandtec/gespospring/model/WorkScheduleModel.java
@@ -1,5 +1,7 @@
 package com.bandtec.gespospring.model;
 
+import com.bandtec.gespospring.entity.table.Employee;
+import com.bandtec.gespospring.entity.table.Project;
 import com.bandtec.gespospring.entity.table.WorkSchedule;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,15 +13,28 @@ import java.util.Date;
 public class WorkScheduleModel {
 
     public WorkScheduleModel(WorkSchedule workSchedule) {
+        Employee emp = new Employee();
+        Project pro = new Project();
+
+        emp.setId(workSchedule.getEmployee().getId());
+        emp.setName(workSchedule.getEmployee().getName());
+
+        pro.setId(workSchedule.getProject().getId());
+        pro.setName(workSchedule.getProject().getName());
+
         this.id = workSchedule.getId();
         this.creationDate = workSchedule.getCreationDate();
         this.amountHours = workSchedule.getAmountHours();
         this.dsWork = workSchedule.getDsWork();
+        this.employee =  emp;
+        this.project = pro;
     }
 
     private Integer id;
     private Date creationDate;
     private Double amountHours;
     private String dsWork;
+    private Employee employee;
+    private Project project;
 
 }
