400890b90b12d8a637dc084317d6c23eb7fe7f66
Victor Massao <victor.massao@cerc.inf.br>
2020-06-07 15:53:51 -0300

    Fixing filter for ProvisioningHours

--- src/main/java/com/bandtec/gespospring/entity/table/ProvisioningHours.java
+++ src/main/java/com/bandtec/gespospring/entity/table/ProvisioningHours.java
@@ -4,9 +4,6 @@ import com.bandtec.gespospring.entity.AbstractModel;
 import lombok.*;
 
 import javax.persistence.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -26,26 +23,4 @@ public class ProvisioningHours extends AbstractModel {
     @JoinColumn(name = "FK_EMPLOYEE")
     private Employee employee;
 
-    public ProvisioningHours(Integer projectId, Integer employeeId, Integer categoryId, String month) {
-        Employee employee = new Employee();
-        Project project = new Project();
-        Category category = new Category();
-
-        category.setId(categoryId);
-        employee.setCategory(category);
-        employee.setId(employeeId);
-        project.setId(projectId);
-
-        this.employee = employee;
-        this.project = project;
-
-        try {
-            if(month != null) {
-                Date formattedDate = new SimpleDateFormat("yyyy-MM-dd").parse(month);
-                this.setCreationDate(formattedDate);
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
 }
